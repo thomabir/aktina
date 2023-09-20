4df270b6f736b95800ad962a4900c5b895ab1444
thomabir <thomabir@phys.ethz.ch>
2023-09-20 13:29:50 +0200

    Change Vector to 4D, add Dir and Point

--- prototype/geometry/vector.hpp
+++ prototype/geometry/vector.hpp
@@ -5,31 +5,37 @@
 
 template <typename T>
 struct Vector {
-  T x, y, z;
-  Vector(T x, T y, T z) : x{x}, y{y}, z{z} {}
+  T x, y, z, w;
+  Vector(T x, T y, T z, T w) : x{x}, y{y}, z{z}, w{w} {}
   Vector() = default;
-  Vector(const Vector<T>& other) : x{other.x}, y{other.y}, z{other.z} {}
+  Vector(const Vector<T>& other)
+      : x{other.x}, y{other.y}, z{other.z}, w{other.w} {}
   Vector& operator=(const Vector<T>& other) {
     if (this != &other) {
       x = other.x;
       y = other.y;
       z = other.z;
+      w = other.w;
     }
     return *this;
   }
 
-  Vector<T> operator-() { return Vector<T>(-x, -y, -z); }
+  bool isPoint() { return w == 1; }
+  bool isDirection() { return w == 0; }
+
+  Vector<T> operator-() { return Vector<T>(-x, -y, -z, -w); }
   T length() { return sqrt(dot(*this, *this)); }
   Vector<T> normalise() {
     T len = this->length();
-    return Vector<T>(this->x / len, this->y / len, this->z / len);
+    return Vector<T>{this->x / len, this->y / len, this->z / len,
+                     this->w / len};
   }
 };
 
 // add
 template <typename T, typename U>
 Vector<T> operator+(Vector<T> t, Vector<U> u) {
-  return Vector<T>{t.x + u.x, t.y + u.y, t.z + u.z};
+  return Vector<T>{t.x + u.x, t.y + u.y, t.z + u.z, t.w + u.w};
 }
 
 // subtract
@@ -41,7 +47,7 @@ Vector<T> operator-(Vector<T> t, Vector<U> u) {
 // scalar multiplication
 template <typename T, typename U>
 Vector<T> operator*(Vector<T> t, U u) {
-  return Vector<T>{t.x * u, t.y * u, t.z * u};
+  return Vector<T>{t.x * u, t.y * u, t.z * u, t.w * u};
 }
 
 template <typename T, typename U>
@@ -52,23 +58,35 @@ Vector<U> operator*(T u, Vector<U> t) {
 // scalar division
 template <typename T, typename U>
 Vector<T> operator/(Vector<T> t, U u) {
-  return Vector<T>{t.x / u, t.y / u, t.z / u};
+  return Vector<T>{t.x / u, t.y / u, t.z / u, t.w / u};
 }
 
 template <typename T, typename U>
 T dot(Vector<T> t, Vector<U> u) {
-  return t.x * u.x + t.y * u.y + t.z * u.z;
+  return t.x * u.x + t.y * u.y + t.z * u.z + t.w * u.w;
 }
 
 template <typename T, typename U>
 Vector<T> cross(Vector<T> t, Vector<U> u) {
   return Vector<T>(t.y * u.z - t.z * u.y, t.z * u.x - t.x * u.z,
-                   t.x * u.y - t.y * u.x);
+                   t.x * u.y - t.y * u.x, 0.0);
 }
 
 template <typename T, typename U>
 bool isApproxEqual(Vector<T> t, Vector<U> u) {
   // compare two Vectors for approximate equality
   return isApproxEqual(t.x, u.x) && isApproxEqual(t.y, u.y) &&
-         isApproxEqual(t.z, u.z);
+         isApproxEqual(t.z, u.z) && isApproxEqual(t.w, u.w);
+}
+
+// Function to initialise a Vector as Direction, with w = 0
+template <typename T>
+Vector<T> direction(T x, T y, T z) {
+  return Vector<T>{x, y, z, 0};
+}
+
+// Function to initialise a Vector as point, with w = 1
+template <typename T>
+Vector<T> point(T x, T y, T z) {
+  return Vector<T>{x, y, z, 1};
 }
--- prototype/geometry/vector.test.cpp
+++ prototype/geometry/vector.test.cpp
@@ -4,121 +4,180 @@
 #include <cmath>
 
 TEST_CASE("Vector") {
-  Vector<float> v1{4.3, -4.2, 3.1};
-  Vector<float> v2{4.3, -4.2, 3.1};
+  Vector<float> v1{4.3, -4.2, 3.1, 0};
+  Vector<float> v2{4.3, -4.2, 3.1, 1};
 
   SECTION("Constructors") {
     // default constructor
     Vector<float> v3;
-    REQUIRE(isApproxEqual(v3, Vector<float>{0, 0, 0}));
+    REQUIRE(isApproxEqual(v3, Vector<float>{0., 0., 0., 0.}));
 
     // component-wise constructor
-    REQUIRE(isApproxEqual(v1, Vector<float>{4.3, -4.2, 3.1}));
+    REQUIRE(isApproxEqual(v1, Vector<float>{4.3, -4.2, 3.1, 0}));
 
     // copy constructor
     Vector<float> v4{v1};
-    REQUIRE(isApproxEqual(v4, Vector<float>{4.3, -4.2, 3.1}));
+    REQUIRE(isApproxEqual(v4, Vector<float>{4.3, -4.2, 3.1, 0}));
 
     // copy assignment operator
     Vector<float> v5;
     v5 = v1;
-    REQUIRE(isApproxEqual(v5, Vector<float>{4.3, -4.2, 3.1}));
+    REQUIRE(isApproxEqual(v5, Vector<float>{4.3, -4.2, 3.1, 0}));
   }
 
   SECTION("Equality") {
-    REQUIRE(isApproxEqual(v1, Vector<double>{4.3, -4.2, 3.1}));
-    REQUIRE(isApproxEqual(v2, Vector<double>{4.3, -4.2, 3.1}));
+    REQUIRE(isApproxEqual(v1, Vector<double>{4.3, -4.2, 3.1, 0}));
+    REQUIRE(isApproxEqual(v2, Vector<double>{4.3, -4.2, 3.1, 1}));
 
-    REQUIRE(!isApproxEqual(v1, Vector<double>{4.3, -4.2, 3.2}));
+    REQUIRE(!isApproxEqual(v1, Vector<double>{4.3, -4.2, 3.2, 0}));
+  }
+
+  SECTION("Check Point or Direction") {
+    REQUIRE(v1.isDirection());
+    REQUIRE(!v1.isPoint());
+    REQUIRE(!v2.isDirection());
+    REQUIRE(v2.isPoint());
   }
 
   SECTION("Component") {
     REQUIRE(isApproxEqual(v1.x, 4.3));
     REQUIRE(isApproxEqual(v1.y, -4.2));
     REQUIRE(isApproxEqual(v1.z, 3.1));
+    REQUIRE(isApproxEqual(v1.w, 0));
 
     REQUIRE(isApproxEqual(v2.x, 4.3));
     REQUIRE(isApproxEqual(v2.y, -4.2));
     REQUIRE(isApproxEqual(v2.z, 3.1));
+    REQUIRE(isApproxEqual(v2.w, 1));
   }
 
   SECTION("Addition") {
-    Vector<double> a1{3, -2, 5};
-    Vector<double> a2{-2, 3, 1};
+    Vector<double> a1{3, -2, 5, 1};
+    Vector<double> a2{-2, 3, 1, 0};
     auto sum = a1 + a2;
-    REQUIRE(isApproxEqual(sum, Vector<double>{1, 1, 6}));
+    REQUIRE(isApproxEqual(sum, Vector<double>{1, 1, 6, 1}));
   }
 
   SECTION("Subtraction") {
-    Vector<double> a1{3, 2, 1};
-    Vector<double> a2{5, 6, 7};
+    Vector<double> a1{3, 2, 1, 4};
+    Vector<double> a2{5, 6, 7, 8};
     auto diff = a1 - a2;
-    REQUIRE(isApproxEqual(diff, Vector<double>{-2, -4, -6}));
+    REQUIRE(isApproxEqual(diff, Vector<double>{-2, -4, -6, -4}));
   }
 
   SECTION("Unary minus") {
-    Vector<double> a1{1, -2, 3};
+    Vector<double> a1{1, -2, 3, -4};
     auto neg = -a1;
-    REQUIRE(isApproxEqual(neg, Vector<double>{-1, 2, -3}));
+    REQUIRE(isApproxEqual(neg, Vector<double>{-1, 2, -3, 4}));
   }
 
   SECTION("Scalar multiplication") {
-    Vector<double> a1{1, -2, 3};
+    Vector<double> a1{1, -2, 3, -4};
     auto prod = a1 * 3.5;
-    REQUIRE(isApproxEqual(prod, Vector<double>{3.5, -7, 10.5}));
+    REQUIRE(isApproxEqual(prod, Vector<double>{3.5, -7, 10.5, -14}));
   }
 
   SECTION("Scalar division") {
-    Vector<double> a1{1, -2, 3};
+    Vector<double> a1{1, -2, 3, -4};
     auto quot = a1 / 2;
-    REQUIRE(isApproxEqual(quot, Vector<double>{0.5, -1, 1.5}));
+    REQUIRE(isApproxEqual(quot, Vector<double>{0.5, -1, 1.5, -2}));
+  }
+}
+
+TEST_CASE("Point") {
+  auto p1 = point(4.0, -4.0, 3.0);
+  auto p2 = point(1.0, 2.0, 3.0);
+
+  REQUIRE(isApproxEqual(p1, Vector<double>{4.0, -4.0, 3.0, 1.0}));
+  REQUIRE(p1.isPoint());
+  REQUIRE(!p1.isDirection());
+
+  SECTION("Subtraction") {
+    auto diff = p1 - p2;
+    REQUIRE(isApproxEqual(diff, direction(3.0, -6.0, 0.0)));
+    REQUIRE(diff.isDirection());
+  }
+}
+
+TEST_CASE("Direction") {
+  auto d1 = direction(3.0, 2.0, 1.0);
+  auto d2 = direction(5.0, 6.0, 7.0);
+
+  SECTION("Equality") {
+    REQUIRE(isApproxEqual(d1, direction(3.0, 2.0, 1.0)));
+    REQUIRE(isApproxEqual(d1, Vector<double>{3.0, 2.0, 1.0, 0.0}));
+  }
+
+  REQUIRE(!d1.isPoint());
+  REQUIRE(d1.isDirection());
+
+  SECTION("Addition") {
+    auto sum = d1 + d2;
+    REQUIRE(isApproxEqual(sum, Vector<double>{8.0, 8.0, 8.0, 0.0}));
+    REQUIRE(isApproxEqual(sum, direction(8.0, 8.0, 8.0)));
+    REQUIRE(sum.isDirection());
+  }
+
+  SECTION("Subtraction") {
+    auto diff = d1 - d2;
+    REQUIRE(isApproxEqual(diff, Vector<double>{-2.0, -4.0, -6.0, 0.0}));
+    REQUIRE(isApproxEqual(diff, direction(-2.0, -4.0, -6.0)));
+    REQUIRE(diff.isDirection());
   }
 
   SECTION("Length") {
-    Vector<double> v1{1, 0, 0};
-    REQUIRE(isApproxEqual(v1.length(), 1.0));
+    auto d0 = direction(0.0, 0.0, 0.0);
+    REQUIRE(isApproxEqual(d0.length(), 0.0));
 
-    Vector<double> v2{0, 1, 0};
-    REQUIRE(isApproxEqual(v2.length(), 1.0));
+    auto d3 = direction(1.0, 0.0, 0.0);
+    REQUIRE(isApproxEqual(d3.length(), 1.0));
 
-    Vector<double> v3{0, 0, 1};
-    REQUIRE(isApproxEqual(v3.length(), 1.0));
+    d0 = direction(0.0, 1.0, 0.0);
+    REQUIRE(isApproxEqual(d0.length(), 1.0));
 
-    Vector<double> v4{1, 2, 3};
-    REQUIRE(isApproxEqual(v4.length(), sqrt(14)));
+    d1 = direction(0.0, 0.0, 1.0);
+    REQUIRE(isApproxEqual(d1.length(), 1.0));
 
-    Vector<double> v5{-1, -2, -3};
-    REQUIRE(isApproxEqual(v5.length(), sqrt(14)));
+    d2 = direction(1.0, 2.0, 3.0);
+    REQUIRE(isApproxEqual(d2.length(), sqrt(14)));
 
-    Vector<double> v6{0, 0, 0};
-    REQUIRE(isApproxEqual(v6.length(), 0.0));
+    d3 = direction(-1.0, -2.0, -3.0);
+    REQUIRE(isApproxEqual(d3.length(), sqrt(14)));
   }
 
   SECTION("Normalise") {
-    Vector<double> v1{4, 0, 0};
-    REQUIRE(isApproxEqual(v1.normalise(), Vector<double>{1, 0, 0}));
+    d1 = direction(4.0, 0.0, 0.0);
+    REQUIRE(isApproxEqual(d1.normalise(), direction(1.0, 0.0, 0.0)));
 
-    Vector<double> v2{1, 2, 3};
+    d1 = direction(1.0, 2.0, 3.0);
     REQUIRE(isApproxEqual(
-        v2.normalise(),
-        Vector<double>{1 / sqrt(14), 2 / sqrt(14), 3 / sqrt(14)}));
+        d1.normalise(),
+        direction(1.0 / sqrt(14), 2.0 / sqrt(14), 3.0 / sqrt(14))));
   }
 
-  SECTION("Magnitude of a normalised vector") {
-    Vector<double> v1{1, 2, 3};
-    REQUIRE(isApproxEqual(v1.normalise().length(), 1.0));
+  SECTION("Length of a normalised vector") {
+    d1 = direction(1.0, 2.0, 3.0);
+    REQUIRE(isApproxEqual(d1.normalise().length(), 1.0));
   }
 
   SECTION("Dot product") {
-    Vector<double> v1{1, 2, 3};
-    Vector<double> v2{2, 3, 4};
-    REQUIRE(isApproxEqual(dot(v1, v2), 20));
+    d1 = direction(1.0, 2.0, 3.0);
+    d2 = direction(2.0, 3.0, 4.0);
+    REQUIRE(isApproxEqual(dot(d1, d2), 20.0));
   }
 
   SECTION("Cross product") {
-    Vector<double> v1{1, 2, 3};
-    Vector<double> v2{2, 3, 4};
-    REQUIRE(isApproxEqual(cross(v1, v2), Vector<double>{-1, 2, -1}));
-    REQUIRE(isApproxEqual(cross(v2, v1), Vector<double>{1, -2, 1}));
+    d1 = direction(1.0, 2.0, 3.0);
+    d2 = direction(2.0, 3.0, 4.0);
+    REQUIRE(isApproxEqual(cross(d1, d2), direction(-1.0, 2.0, -1.0)));
+    REQUIRE(isApproxEqual(cross(d2, d1), direction(1.0, -2.0, 1.0)));
   }
+}
+
+TEST_CASE("Subtracting a direction from a point") {
+  auto p = point(3.0, 2.0, 1.0);
+  auto d = direction(5.0, 6.0, 7.0);
+  auto diff = p - d;
+  REQUIRE(isApproxEqual(diff, point(-2.0, -4.0, -6.0)));
+  REQUIRE(diff.isPoint());
 }
