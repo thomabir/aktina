6a690bd98d28b70e2452304b7306f7ca586ea602
thomabir <thomabir@phys.ethz.ch>
2023-09-20 19:25:13 +0200

    Add Canvas <<
    TODO:
    - Remove space before newline
    - Add newline after 70 characters

--- aktina/canvas.hpp
+++ aktina/canvas.hpp
@@ -25,6 +25,25 @@ struct Canvas {
     return pixels[y * width + x];
   }
 
+  void clamp() {
+    // multiply all by 255
+    for (int i = 0; i < width * height; ++i) {
+      pixels[i].r *= 255;
+      pixels[i].g *= 255;
+      pixels[i].b *= 255;
+    }
+
+    // clamp to [0, 255]
+    for (int i = 0; i < width * height; ++i) {
+      if (pixels[i].r < 0) pixels[i].r = 0;
+      if (pixels[i].g < 0) pixels[i].g = 0;
+      if (pixels[i].b < 0) pixels[i].b = 0;
+      if (pixels[i].r > 255) pixels[i].r = 255;
+      if (pixels[i].g > 255) pixels[i].g = 255;
+      if (pixels[i].b > 255) pixels[i].b = 255;
+    }
+  }
+
   // println: print in the following format:
   // (i, j) r g b
   void println() {
@@ -42,13 +61,43 @@ struct Canvas {
   // width height
   // 255
   // Then, each pixel is printed in the following format:
-  // r g b
+  // r g b r g b ... (times width)
+  // ...
+  // r g b r g b ... (times width)
+  // where r, g, b are integers in [0, 255].
+
   friend std::ostream& operator<<(std::ostream& os, const Canvas<T>& c) {
+    // ppm header
     os << "P3\n" << c.width << " " << c.height << "\n255\n";
+
+    // clamp the canvas
+    Canvas<T> c2 = c;
+    c2.clamp();
+
+    // round to the nearest integer, using an int Canvas
+    Canvas<int> c3(c.width, c.height);
     for (int i = 0; i < c.width; ++i) {
       for (int j = 0; j < c.height; ++j) {
-        os << c(i, j).r << " " << c(i, j).g << " " << c(i, j).b << "\n";
+        c3(i, j).r = std::round(c2(i, j).r);
+        c3(i, j).g = std::round(c2(i, j).g);
+        c3(i, j).b = std::round(c2(i, j).b);
       }
+    }
+
+    // print the pixels in the format r g b r g b ...
+    // until either the width of the canvas is reached, or
+    // the maximum line length of 70 characters is reached.
+    for (int j = 0; j < c.height; ++j) {
+      int line_length = 0;
+      for (int i = 0; i < c.width; ++i) {
+        os << c3(i, j).r << " " << c3(i, j).g << " " << c3(i, j).b << " ";
+        line_length += 12;
+        if (line_length >= 70) {
+          os << "\n";
+          line_length = 0;
+        }
+      }
+      os << "\n";
     }
     return os;
   }
--- aktina/canvas.test.cpp
+++ aktina/canvas.test.cpp
@@ -1,6 +1,7 @@
 #include "canvas.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <sstream>
 
 TEST_CASE("Canvas") {
   SECTION("Constructors") {
@@ -25,5 +26,45 @@ TEST_CASE("Canvas") {
         }
       }
     }
+  }
+
+  SECTION("Clamp") {
+    Canvas<float> c(10, 20);
+    c(2, 3) = Color<float>{-1.0f, 0.5f, 1.3f};
+    c.clamp();
+    REQUIRE(isApproxEqual(c(2, 3), Color<float>{0.0f, 127.5f, 255.0f}));
+  }
+
+  SECTION("Print to file") {
+    Canvas<float> c(5, 3);
+    c(0, 0) = Color<float>{1.5f, 0.0f, 0.0f};
+    c(2, 1) = Color<float>{0.0f, 0.5f, 0.0f};
+    c(4, 2) = Color<float>{-0.5f, 0.0f, 1.0f};
+    std::stringstream ss;
+    ss << c;
+    REQUIRE(ss.str() ==
+            "P3\n5 3\n255\n"
+            "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
+            "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0\n"
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255\n");
+  }
+
+  SECTION("Print to file with max. 70 characters per line") {
+    Canvas<float> c(10, 2);
+    for (int i = 0; i < 10; ++i) {
+      for (int j = 0; j < 2; ++j) {
+        c(i, j) = Color<float>{1.0f, 0.8f, 0.6f};
+      }
+    }
+    std::stringstream ss;
+    ss << c;
+    REQUIRE(ss.str() ==
+            "P3\n10 2\n255\n"
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 "
+            "255 204\n"
+            "153 255 204 153 255 204 153 255 204 153 255 204 153\n"
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 "
+            "255 204\n"
+            "153 255 204 153 255 204 153 255 204 153 255 204 153\n");
   }
 }
